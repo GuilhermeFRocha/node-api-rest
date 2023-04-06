ac34731c972aa051d00edb25e4da6d9814d4f4d4
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2023-04-06 14:20:13 -0300

    add-eslint

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.get("/hello", () => {
-  return "Hello";
-});
+app.get('/hello', () => {
+  return 'Hello'
+})
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("Running!");
-  });
+    console.log('Hello')
+  })
